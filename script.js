7860d51911e17039e43ce9ca741623aa237c4e48
Blynk <david.obatake@gmail.com>
2016-10-15 19:23:11 -0500

    some finishing touches for now

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+	$('.project-heading').append('<span class="span1" style="float:right;">&#9660;</span>')
+
 	$('.project-heading').click(function(){
-		$(this).next().toggle();
+		if($(this).find('.span1').attr('id') == 'yes') {
+            $(this).find('.span1').attr('id', '').html('&#9660;');
+        } else {
+            $(this).find('.span1').attr('id', 'yes').html('&#9650;');
+        }
+        $(this).next('.project-description').toggle();
 	});
 
 	$(".nav li a").click(function(){
